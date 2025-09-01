54fd73876d3fe15c4eedae5b211d5478a3152ca9
DevelopJanerson <janerson_roger@hotmail.com>
2025-08-31 21:52:08 -0300

    correção

--- src/server.js
+++ src/server.js
@@ -1,34 +1,38 @@
 const express = require('express');
-const os = require('os')
+const os = require('os');
 const app = express();
-const conversor = require('./convert')
+const conversor = require('./convert');
 const bodyParser = require('body-parser');
 const config = require('./config/system-life');
 const path = require('path');
 
+// Prometheus client
+const client = require('prom-client');
+const register = client.register;
+
+// Coleta de métricas padrão (CPU, memória, etc.)
+client.collectDefaultMetrics();
+
 app.use(config.middlewares.healthMid);
 app.use('/', config.routers);
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/fahrenheit/:valor/celsius', (req, res) => {
-
     let valor = req.params.valor;
     let celsius = conversor.fahrenheitCelsius(valor);
     res.json({ "celsius": celsius, "maquina": os.hostname() });
 });
 
 app.get('/celsius/:valor/fahrenheit', (req, res) => {
-
     let valor = req.params.valor;
     let fahrenheit = conversor.celsiusFahrenheit(valor);
     res.json({ "fahrenheit": fahrenheit, "maquina": os.hostname() });
 });
 
 app.get('/', (req, res) => {
-
-    res.render('index',{valorConvertido: '', maquina: os.hostname()});
+    res.render('index', { valorConvertido: '', maquina: os.hostname() });
 });
 
 app.post('/', (req, res) => {
@@ -36,21 +40,26 @@ app.post('/', (req, res) => {
 
     if (req.body.valorRef) {
         if (req.body.selectTemp == 1) {
-            resultado = conversor.celsiusFahrenheit(req.body.valorRef)
+            resultado = conversor.celsiusFahrenheit(req.body.valorRef);
         } else {
-            resultado = conversor.fahrenheitCelsius(req.body.valorRef)
+            resultado = conversor.fahrenheitCelsius(req.body.valorRef);
         }
     }
 
-    res.render('index', {valorConvertido: resultado, "maquina": os.hostname()});
- });
+    res.render('index', { valorConvertido: resultado, "maquina": os.hostname() });
+});
 
- // Endpoint para métricas do Prometheus
+// Endpoint de métricas Prometheus
 app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (err) {
+        res.status(500).end(err);
+    }
 });
 
 app.listen(8080, () => {
     console.log("Servidor rodando na porta 8080");
 });
+
